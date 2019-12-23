8e92f07cd5622f5a82b30b36688767cd41d5536b
Camille-Dev <camille.theil@gmail.com>
2019-12-23 19:12:15 +0100

    feature: Countdown 24h ok!

--- app/components/my-countdown.js
+++ app/components/my-countdown.js
@@ -2,5 +2,15 @@ import countdown from 'countdownjs';
 import CountdownString from 'ember-countdown/components/countdown-string';
 
 export default CountdownString.extend({
-
+  didReceiveAttrs() {
+    this._super(...arguments);
+    let today = new Date();
+    let tomorrow = new Date(today)
+    tomorrow.setDate(tomorrow.getDate() + 1)
+    let dd = String(tomorrow.getDate()).padStart(2, '0');
+    let mm = String(tomorrow.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = tomorrow.getFullYear();
+    tomorrow = mm + '/' + dd + '/' + yyyy + " 00:00:00 GMT";
+    this.set("startDate", tomorrow)
+  }
 });
