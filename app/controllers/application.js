86d6ca34b381ab786d96add08c24f1737f388834
johannabzg <johanna.bouzige@gmail.com>
2019-12-28 23:35:25 +0100

    Add modal

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,11 +2,14 @@ import Controller from '@ember/controller';
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isShowingModalu: false,
   isShowingModal: false,
   actions: {
     toggleModal: function() {
       this.toggleProperty('isShowingModal');
+    },
+    toggleModalu: function() {
+      this.toggleProperty('isShowingModalu');
     }
   }
 });
-
